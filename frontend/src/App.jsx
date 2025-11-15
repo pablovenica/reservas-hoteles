f130544e159157e05164786f1a1cf211acdacb36
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-11-15 19:15:52 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// === PAGES ===
 import { Home } from "./pages/Home";
 import { Usuario } from "./pages/Usuario";
 import { Reservas } from "./pages/Reservas";
@@ -9,30 +8,18 @@ import { Admin } from "./pages/Admin";
 import { Login } from "./pages/Login";
 import { Registro } from "./pages/Registro";
 
-// === ESTILOS ===
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* === PÁGINA PRINCIPAL === */}
         <Route path="/" element={<Home />} />
-
-        {/* === SECCIÓN USUARIO === */}
         <Route path="/usuario" element={<Usuario />} />
         <Route path="/usuario/reservas" element={<Reservas />} />
-
-        {/* === PANEL ADMIN === */}
         <Route path="/admin" element={<Admin />} />
-
-        {/* === LOGIN === */}
         <Route path="/login" element={<Login />} />
-
-        {/* === REGISTRO === */}
         <Route path="/registro" element={<Registro />} />
-
-        {/* === ERROR 404 === */}
         <Route
           path="*"
           element={
