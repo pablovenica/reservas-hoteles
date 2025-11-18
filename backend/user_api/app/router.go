52e058cc0201c164fd8f84246d8b0074318cee5c
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-11-18 15:43:03 -0300

    Update router.go

--- backend/user_api/app/router.go
+++ backend/user_api/app/router.go
@@ -17,7 +17,7 @@ func init() {
 
 	router.Use(cors.New(cors.Config{
 
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
