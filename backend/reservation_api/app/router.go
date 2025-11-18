73b59a1f010ab4783f361a99f2f2fe8b62ad97a9
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-11-18 15:42:43 -0300

    Update CORS allowed origins for the API

--- backend/reservation_api/app/router.go
+++ backend/reservation_api/app/router.go
@@ -16,7 +16,7 @@ func init(){
 
 	router.Use(cors.New(cors.Config{
 		
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:5173"},
 		AllowMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-type", "Authorization"},
 		AllowCredentials:true,
