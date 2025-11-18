ca8a012a40af8674d94f576fd21cd0e717ef08b3
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-11-18 18:50:17 -0300

    Delete backend directory

--- backend/user_api/dto/user_dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type UserDto struct {
-	ID          int    `json:"id_usuarios"`
-	Nombre      string `json:"nombre"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	TipoUsuario string `json:"tipo_usuario"`
-}
